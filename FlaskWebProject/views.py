5984f3ed8953a8a10ee056c08f55d721e9926552
Глеб Боушев < >
2016-08-30 21:01:17 +0300

    okgo take2

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -1,15 +1,9 @@
-"""
-Routes and views for the flask application.
-"""
-
 import pyodbc, os
 from datetime import datetime
 from flask import render_template,url_for,request,redirect,session,flash
 from FlaskWebProject import app
 
-
-@app.route('/')
-@app.route('/home', methods=['get','post'])
+@app.route('/', methods=['get','post'])
 def home():
 
     cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=%s;PORT=1433;DATABASE=%s;UID=%s;PWD=%s' % (os.getenv('SQL_ADR'), os.getenv('SQL_DTB'), os.getenv('SQL_USR'), os.getenv('SQL_PWD')))
@@ -28,7 +22,7 @@ def home():
             data=row
         )
 
-    cursor.execute("select * from wp_terms")
+    cursor.execute("select * from DataTable")
     row = cursor.fetchall()
 
     """Renders the home page."""
