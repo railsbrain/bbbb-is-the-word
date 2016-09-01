003f5885e96a8a3b217fc0a5f835e1019466c5b5
Глеб Боушев < >
2016-09-01 20:24:35 +0300

    testing azure

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -4,13 +4,11 @@ from flask import render_template,url_for,request,redirect,session,flash
 from FlaskWebProject import app
 from azure.storage.queue import QueueService
 
-queue_service = QueueService(account_name='bbbb', account_key='/c34iMU0/yixxAxVTO9cOLZu4JA1YT0jPKWdtxRG3gGfZ7A7Sw0cXKT0cNNtokLcOa4wG/4ux1VjsWfXNdJWCA==')
-queue_service.create_queue('bbbb')
-
 @app.route('/', methods=['get','post'])
 def home():
 
-
+    queue_service = QueueService(account_name='bbbb', account_key='/c34iMU0/yixxAxVTO9cOLZu4JA1YT0jPKWdtxRG3gGfZ7A7Sw0cXKT0cNNtokLcOa4wG/4ux1VjsWfXNdJWCA==')
+    queue_service.create_queue('bbbb')
 
     cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=%s;PORT=1433;DATABASE=%s;UID=%s;PWD=%s' % (os.getenv('SQL_ADR'), os.getenv('SQL_DTB'), os.getenv('SQL_USR'), os.getenv('SQL_PWD')))
     cursor = cnxn.cursor()
@@ -37,13 +35,4 @@ def home():
         title='Home Page',
         year=datetime.now().year,
         data=row
-    )
-    
-    
-@app.route('/awesome')
-def awesome():
-    """Renders awesome"""
-    return render_template(
-        'awesome.html',
-        title='AWESOME',
     )
