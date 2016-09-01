89d21b05816bdbaf4961e089ba27e713cf50e1d3
Глеб Боушев < >
2016-09-01 20:29:18 +0300

    removing db stuff

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -2,16 +2,12 @@ import pyodbc, os
 from datetime import datetime
 from flask import render_template,url_for,request,redirect,session,flash
 from FlaskWebProject import app
-'''from azure.storage.queue import QueueService'''
 
 @app.route('/', methods=['get','post'])
 def home():
 
-    queue_service = QueueService(account_name='bbbb', account_key='/c34iMU0/yixxAxVTO9cOLZu4JA1YT0jPKWdtxRG3gGfZ7A7Sw0cXKT0cNNtokLcOa4wG/4ux1VjsWfXNdJWCA==')
-    queue_service.create_queue('bbbb')
-
-    cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=%s;PORT=1433;DATABASE=%s;UID=%s;PWD=%s' % (os.getenv('SQL_ADR'), os.getenv('SQL_DTB'), os.getenv('SQL_USR'), os.getenv('SQL_PWD')))
-    cursor = cnxn.cursor()
+    #cnxn = pyodbc.connect('DRIVER={SQL Server};SERVER=%s;PORT=1433;DATABASE=%s;UID=%s;PWD=%s' % (os.getenv('SQL_ADR'), os.getenv('SQL_DTB'), os.getenv('SQL_USR'), os.getenv('SQL_PWD')))
+    #cursor = cnxn.cursor()
 
     if request.method == "POST":
         data = request.form['data']
@@ -26,10 +22,9 @@ def home():
             data=row
         )
 
-    cursor.execute("select * from DataTable")
-    row = cursor.fetchall()
+    #cursor.execute("select * from DataTable")
+    #row = cursor.fetchall()
 
-    """Renders the home page."""
     return render_template(
         'index.html',
         title='Home Page',
