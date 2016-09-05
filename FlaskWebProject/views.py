23e4c70e2614f0e5ba5601cf7160c48c50f293cb
Глеб Боушев < >
2016-09-06 00:12:19 +0300

    push!

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -1,11 +1,11 @@
-import os,azure.storage
+import os
 from datetime import datetime
 from flask import render_template,url_for,request,redirect,session,flash
 from FlaskWebProject import app
-#from azure.storage.queue import QueueService
+from azure.storage.queue import QueueService
 
-#queue_service = QueueService(account_name='bbbb', account_key='Do2+kHErXe41U38w91iiBMDTkHNceRmVhYT3YGNkvO+jj1BfKoLVGYaDn+wGdPw39NwzJNHmDSj3Frzm5xOF7Q==')
-#queue_service.create_queue('taskqueue1')
+queue_service = QueueService(account_name='bbbb', account_key='Do2+kHErXe41U38w91iiBMDTkHNceRmVhYT3YGNkvO+jj1BfKoLVGYaDn+wGdPw39NwzJNHmDSj3Frzm5xOF7Q==')
+queue_service.create_queue('cccc')
 
 @app.route('/', methods=['get','post'])
 def home():
