cf05627ace04da796be4372035a803704bb183cd
Глеб Боушев < >
2016-09-10 20:17:58 +0300

    fixed some stuff

--- ARM/process.ps1
+++ ARM/process.ps1
@@ -1,24 +1,33 @@
-#ready
-New-AzureRmResourceGroup –Name 'bbbb' –Location 'Northeurope'
-New-AzureRmKeyVault -VaultName 'bbbbault' -ResourceGroupName 'bbbb' -Location 'Northeurope'
-Set-AzureRmKeyVaultAccessPolicy -EnabledForTemplateDeployment -VaultName 'bbbbault'
-#set
+#¿wha?
+$resourceGroupName = 'bbbb'
+$location = 'northeurope'
+$vautlName = 'bbbbault'
+
+#¡ready
+New-AzureRmResourceGroup –Name $resourceGroupName –Location $location
+New-AzureRmKeyVault -VaultName $vautlName -ResourceGroupName $resourceGroupName -Location $location
+Set-AzureRmKeyVaultAccessPolicy -EnabledForTemplateDeployment -VaultName $vautlName
+
+#set!
 $secretvalue = ConvertTo-SecureString '!Q2w3e4r' -AsPlainText -Force
 $secret = Set-AzureKeyVaultSecret -VaultName 'bbbbault' -Name 'administratorLoginPassword' -SecretValue $secretvalue
-#go
-$path = "C:\git\bbbb-is-the-word\ARM\parent.json"
+
+#¡go!
 $parameters = @{
+    'Name' = 'bbbb-is-the-word'   
+    'ResourceGroupName' = $resourceGroupName
+    "TemplateUri"="https://raw.githubusercontent.com/4c74356b41/bbbb-is-the-word/master/ARM/parent.json"
     "SITENAME-PRIMARY"="bbbb-1s-the-word"
     "SITENAME-SECONDARY"="bbbb-1s-the-wordd"
-    "HOSTINGPLAN-PRIMARY"="h-Plan-a"
-    "HOSTINGPLAN-SECONDARY"="h-Plan-b"
+    "HOSTINGPLAN-PRIMARY"="Plan-a"
+    "HOSTINGPLAN-SECONDARY"="Plan-b"
     "SKU"="F1"
     "ADMINISTRATORLOGIN"="dba"
     "SECRETNAME"="administratorLoginPassword"
     "DATABASENAME"="database"
     "SERVERNAME"="bbbb-sq1-pr1"
     "SERVERREPLICANAME"="bbbb-sq1-rep1"
-    "STORAGEACCOUNTNAME"="bbbb-storage-acc7"
+    "STORAGEACCOUNTNAME"="bbbbistheword"
 }
-New-AzureRmResourceGroupDeployment -Name _ -ResourceGroupName bbbb -TemplateFile $path -TemplateParameterObject $parameters
-#finish
+New-AzureRmResourceGroupDeployment @parameters
+# >>finish<<
