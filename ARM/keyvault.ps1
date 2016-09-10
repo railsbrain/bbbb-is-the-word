dc5fd064c1a5be3806b319ef9b7b1ee872410a83
Глеб Боушев < >
2016-09-10 18:45:40 +0300

    gooo!

--- ARM/keyvault.ps1
+++ ARM/keyvault.ps1
@@ -1,4 +1,8 @@
 New-AzureRmResourceGroup –Name 'bbbb' –Location 'Northeurope'
 New-AzureRmKeyVault -VaultName 'bbbbault' -ResourceGroupName 'bbbb' -Location 'Northeurope'
 Set-AzureRmKeyVaultAccessPolicy -EnabledForTemplateDeployment -VaultName 'bbbbault'
-$key = Add-AzureKeyVaultKey -VaultName 'bbbbault' -Name "administratorLoginPassword" -Destination Software
+
+
+
+$secretvalue = ConvertTo-SecureString '!Q2w3e4r' -AsPlainText -Force
+$secret = Set-AzureKeyVaultSecret -VaultName 'bbbbault' -Name 'administratorLoginPassword' -SecretValue $secretvalue
